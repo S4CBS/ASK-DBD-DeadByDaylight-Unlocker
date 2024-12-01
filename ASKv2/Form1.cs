e3b1a79fbe5181beb1e8c32a6331e84755e0d5d4
Alexey <107351385+S4CBS@users.noreply.github.com>
2024-12-01 22:08:40 +0300

    update_to_V2

---
+++ ASKv2/Form1.cs
@@ -0,0 +1,71 @@
+namespace ASKv2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string prestigeLevel = Utils.InitializeLvLPrestige();
+            Logs.View = View.Details;
+            Logs.Columns.Add("                                               Logs", 350);
+            textBox2.Text = prestigeLevel;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox2.Text;
+            if (int.TryParse(inputText, out int text))
+            {
+                Utils.updatePrestigeLvL(inputText);
+                MessageBox.Show($"ѕрестиж персонажей успешно изменен на: {inputText}", "»зменение престижа");
+                Logs.Items.Add($"ѕрестиж персонажей успешно изменен на: {inputText}", "»зменение престижа");
+            }
+            else
+            {
+                MessageBox.Show($"LVL может быть только числом!", "»зменение престижа", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Logs.Items.Add($"LVL может быть только числом!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.Start(true);
+            Logs.Items.Add("Unlocker Launched!");
+            string StartStopLog = Program.ReturnStartSignal(true);
+            Logs.Items.Add(StartStopLog);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Program.Start(false);
+            Logs.Items.Add("Unlocker Stoped!");
+            string StartStopLog = Program.ReturnStartSignal(false);
+            Logs.Items.Add(StartStopLog);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ ASKv2/Utils.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASKv2
+{
+    internal class Utils
+    {
+        private static string ProfilePath = Path.Combine(Program.ProfilePath, "Bloodweb.json");
+        public static void updatePrestigeLvL(string lvl)
+        {
+            string JSON = File.ReadAllText(ProfilePath);
+            var SettingsObj = JsonConvert.DeserializeObject<Dictionary<string, object>>(JSON);
+
+            if (SettingsObj.ContainsKey("prestigeLevel"))
+            {
+                SettingsObj["prestigeLevel"] = lvl;
+            }
+
+            string FinalJson = JsonConvert.SerializeObject(SettingsObj);
+            File.WriteAllText(ProfilePath, FinalJson);
+        }
+        public static string InitializeLvLPrestige()
+        {
+            string JSON = File.ReadAllText(ProfilePath);
+            var SettingsObj = JsonConvert.DeserializeObject<Dictionary<string, object>>(JSON);
+
+            if (SettingsObj.ContainsKey("prestigeLevel"))
+            {
+                var prestigeLvL = SettingsObj["prestigeLevel"].ToString();
+
+                if (!string.IsNullOrEmpty(prestigeLvL)) {
+                    return prestigeLvL;
+                }
+            }
+            return "0";
+        }
+    }
+}
